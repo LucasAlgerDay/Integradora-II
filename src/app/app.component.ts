4482596b82688bdd7844202adaee5c5b16d94b2e
Cesar Aaron Trejo Riojas <129459121+Trejo342@users.noreply.github.com>
2023-04-17 20:40:03 -0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
     { title: 'Home', url: '/home', icon: 'home' },
     { title: 'Registro', url: '/registro', icon: 'log-out' },
     { title: 'Reloj', url: '/reloj', icon: 'timer' },
+    { title: 'RegistroM', url: '/registro-m', icon: 'log-in' },
   ];
   constructor() {}
 }
